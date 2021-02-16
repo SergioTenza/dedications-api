6f4f23d9eeac53cddc093594ccf268273471a343
Sergio Tenza <43845783+SergioTenza@users.noreply.github.com>
2021-02-16 12:57:13 +0100

    Base Route Message

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const app = express();
 
 //Global Variables
 dotenv.config();
-app.set('port', process.env.PORT || 5000);
+app.set('port', process.env.PORT || 5001);
 
 
 createRoles();
@@ -34,5 +34,12 @@ app.use('/api/tasks', tasksRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/users', usersRoutes);
 app.use('/api/machine', machineRoutes);
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Bienvenido a la Api Dedications By FunPhotosSystems',
+        email: 'Si quiere trabajar con nosotros pongase en contacto con tnzservicios@gmail.com'
+    });
+});
 
 export default app;
